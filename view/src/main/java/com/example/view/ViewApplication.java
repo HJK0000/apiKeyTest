1d125cdb1a44f66d87277a7d3188a7b634454419
HJK0000 <queen00220@naver.com>
2024-07-18 12:45:36 +0900

    프록시

--- view/src/main/java/com/example/view/ViewApplication.java
+++ view/src/main/java/com/example/view/ViewApplication.java
@@ -2,6 +2,8 @@ package com.example.view;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ViewApplication {
@@ -9,5 +11,11 @@ public class ViewApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ViewApplication.class, args);
 	}
+	
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+	
 
 }
--- view/src/main/java/com/example/view/controller/MainController.java
+++ view/src/main/java/com/example/view/controller/MainController.java
@@ -1,11 +1,46 @@
 package com.example.view.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
 
 @Controller
 public class MainController {
 
+	@Value("${google.maps.api.key}")
+	private String googleMapsApiKey;
+	
+	private final RestTemplate restTemplate;
+	
+	
+	
+	public MainController(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 
+
+
+	@GetMapping("/")
+	public String getMap() {
+		return "index";
+		
+	}
+	
+	@GetMapping("/proxy/maps-api")
+	public ResponseEntity<String> proxyMapsApi(@RequestParam Map<String, String> params) {
+        String url = "https://maps.googleapis.com/maps/api/js?key=" + googleMapsApiKey;
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            url += "&" + entry.getKey() + "=" + entry.getValue();
+        }
+        return restTemplate.getForEntity(url, String.class);
+    }
+	
 }
